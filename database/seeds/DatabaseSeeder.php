2c2e32c496d3dd070118a8f617f48eaab5911dc3
Elliot Pettingale <elipettingale@gmail.com>
2017-06-27 09:19:59 +0100

    Update database seeder with users and posts

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\User;
+use App\Post;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,6 +13,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        User::create([
+            'name' => 'Elliot Pettingale',
+            'username' => 'elipettingale',
+            'email' => 'elipettingale@gmail.com',
+            'password' => bcrypt('password'),
+        ]);
+
+        $users = factory(User::class, 20)->create();
+        $posts = factory(Post::class, 100)->create();
     }
 }
