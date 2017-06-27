c966eaa27e3c6e9ba58164f043d860cc0f5b6ea3
Elliot Pettingale <elipettingale@gmail.com>
2017-06-27 13:38:31 +0100

    Create form to submit posts

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,6 +1,12 @@
 @extends('layouts.app')
 
 @section('content')
+    <form method="post">
+        {{ csrf_field() }}
+        <div class="form-group">
+            <input type="text" class="form-control" name="body" placeholder="New Post">
+        </div>
+    </form>
     <div class="panel panel-default">
         @include('snippets.post_list')
     </div>
