07d85c76e290c9d715ce487938789430d10a8371
Elliot Pettingale <elipettingale@gmail.com>
2017-06-27 11:29:34 +0100

    Update post view templates

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -3,7 +3,32 @@
 @section('content')
     <div class="container">
         <div class="row">
-
+            <div class="col-md-3 hidden-sm hidden-xs">
+                <div class="panel panel-default">
+                    <li class="list-group-item">Sidebar</li>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="panel panel-default">
+                    <li class="list-group-item">
+                        <p><a>Author</a> | Timestamp</p>
+                        <p>Body</p>
+                    </li>
+                    <li class="list-group-item">
+                        <p>Author | Timestamp</p>
+                        <p>Body</p>
+                    </li>
+                    <li class="list-group-item">
+                        <p>Author | Timestamp</p>
+                        <p>Body</p>
+                    </li>
+                </div>
+            </div>
+            <div class="col-md-3 hidden-sm hidden-xs">
+                <div class="panel panel-default">
+                    <li class="list-group-item">Sidebar</li>
+                </div>
+            </div>
         </div>
     </div>
 @endsection
