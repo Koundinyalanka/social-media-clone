b968cfb8093323636f8d275440f235724718227e
Elliot Pettingale <elipettingale@gmail.com>
2017-06-27 09:47:14 +0100

    Add posts to app menu

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -43,7 +43,7 @@
                 <div class="collapse navbar-collapse" id="app-navbar-collapse">
                     <!-- Left Side Of Navbar -->
                     <ul class="nav navbar-nav">
-                        &nbsp;
+                        <li{{ Request::url() == url('/posts') ? ' class=active' : '' }}><a href="{{ url('/posts') }}">Posts</a></li>
                     </ul>
 
                     <!-- Right Side Of Navbar -->
