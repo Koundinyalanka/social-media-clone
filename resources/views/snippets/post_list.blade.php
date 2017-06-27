56369c388d458ad24b5f668010d3411e296516a9
Elliot Pettingale <elipettingale@gmail.com>
2017-06-27 12:05:04 +0100

    Add username to post view

--- resources/views/snippets/post_list.blade.php
+++ resources/views/snippets/post_list.blade.php
@@ -1,6 +1,9 @@
 @foreach($posts as $post)
     <li class="list-group-item">
-        <p><a>{{ $post->user->name }}</a> | {{ $post->created_at->diffForHumans() }}</p>
+        <p>
+            <a href="/profile/{{ $post->user->username }}">{{ $post->user->name }}</a>
+            | <a href="/profile/{{ $post->user->username }}">{{ '@' . $post->user->username }}</a>
+            | {{ $post->created_at->diffForHumans() }}</p>
         <p>{{ $post->body }}</p>
     </li>
 @endforeach
