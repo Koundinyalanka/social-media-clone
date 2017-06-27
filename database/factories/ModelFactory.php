9794adc2baf892b8f566dca0e126e173bfccf8d7
Elliot Pettingale <elipettingale@gmail.com>
2017-06-27 09:19:40 +0100

    Create post factory, update user factory to include username

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -17,8 +17,16 @@ $factory->define(App\User::class, function (Faker\Generator $faker) {
 
     return [
         'name' => $faker->name,
+        'username' => $faker->userName,
         'email' => $faker->unique()->safeEmail,
         'password' => $password ?: $password = bcrypt('secret'),
         'remember_token' => str_random(10),
+    ];
+});
+
+$factory->define(App\Post::class, function (Faker\Generator $faker) {
+    return [
+        'user_id' => App\User::all()->random(1),
+        'body' => $faker->paragraph(50),
     ];
 });
