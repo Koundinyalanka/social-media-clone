9e987070e7140038a85c765eaa91d99ba91f91f1
Elliot Pettingale <elipettingale@gmail.com>
2017-06-27 11:42:03 +0100

    Add profile template to view

--- resources/views/profile/show.blade.php
+++ resources/views/profile/show.blade.php
@@ -26,7 +26,16 @@
             </div>
             <div class="col-md-3 hidden-sm hidden-xs">
                 <div class="panel panel-default">
-                    <li class="list-group-item">Sidebar</li>
+                    <li class="list-group-item">
+                        <img class="img-responsive" src="https://www.rogerbrayrestoration.com/wp-content/uploads/2014/08/Blank-Profile.jpg">
+                    </li>
+                    <li class="list-group-item">
+                        <h3>{{ "Name" }}</h3>
+                        <h5>{{ "@username" }}</h5>
+                    </li>
+                    <li class="list-group-item">
+                        Posts <span class="badge">0</span>
+                    </li>
                 </div>
             </div>
         </div>
