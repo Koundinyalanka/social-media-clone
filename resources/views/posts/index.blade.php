308dacb0e047bff90836d94e0b3ca7c2113d7f5b
Elliot Pettingale <elipettingale@gmail.com>
2017-06-28 09:02:18 +0100

    Refactor layouts for improved readability

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,13 +1,25 @@
-@extends('layouts.main')
+@extends('layouts.app')
 
-@section('body')
-    <form method="post">
-        {{ csrf_field() }}
-        <div class="form-group">
-            <input type="text" class="form-control" name="body" placeholder="New Post">
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-3 hidden-sm hidden-xs">
+
+            </div>
+            <div class="col-md-6">
+                <form method="post">
+                    {{ csrf_field() }}
+                    <div class="form-group">
+                        <input type="text" class="form-control" name="body" placeholder="New Post">
+                    </div>
+                </form>
+                <div class="panel panel-default">
+                    @include('snippets.post_list')
+                </div>
+            </div>
+            <div class="col-md-3 hidden-sm hidden-xs">
+
+            </div>
         </div>
-    </form>
-    <div class="panel panel-default">
-        @include('snippets.post_list')
     </div>
 @endsection
